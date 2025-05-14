a4b2ab729813bed582e561f250edea6673f4bb36
Valentin Gardev <bestmaad@gmail.com>
2025-05-14 18:19:38 +0300

    z

--- 07_Double_Char.py
+++
@@ -1,9 +0,0 @@
-while True:
-    string = input()
-    if string == "End":
-        break
-    elif string == "SoftUni":
-        continue
-    for i in string:
-        print(i * 2, end="")
-    print()
